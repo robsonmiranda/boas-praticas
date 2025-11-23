a56ad05cbd5f68df5fd74322c4318627b4b8dfc2
Robson Miranda <rmatosm@gmail.com>
2025-11-23 18:21:31 -0300

    Commit final

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,8 +1,6 @@
 package br.com.alura;
 
-import br.com.alura.client.ClientHttpConfiguration;
-import br.com.alura.service.AbrigoService;
-import br.com.alura.service.PetService;
+import br.com.alura.command.*;
 
 import java.util.Scanner;
 
@@ -10,43 +8,37 @@ public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
-        ClientHttpConfiguration client = new ClientHttpConfiguration();
-        AbrigoService abrigoService = new AbrigoService(client);
-        PetService petService = new PetService(client);
+        CommandExecutor executor = new CommandExecutor();
         try {
             int opcaoEscolhida = 0;
             while (opcaoEscolhida != 5) {
-                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
-                System.out.println("1 -> Listar abrigos cadastrados");
-                System.out.println("2 -> Cadastrar novo abrigo");
-                System.out.println("3 -> Listar pets do abrigo");
-                System.out.println("4 -> Importar pets do abrigo");
-                System.out.println("5 -> Sair");
+                exibirMenu();
                 String textoDigitado = new Scanner(System.in).nextLine();
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
                 switch (opcaoEscolhida) {
-                    case 1:
-                        abrigoService.listarAbrigos();
-                        break;
-                    case 2:
-                        abrigoService.cadastrarAbrigo();
-                        break;
-                    case 3:
-                        petService.listarPets();
-                        break;
-                    case 4:
-                        petService.importarPets();
-                        break;
-                    case 5:
-                        break;
-                    default:
+                    case 1 -> executor.executeCommand(new ListarAbrigoCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsCommand());
+                    case 5 -> System.exit(0);
+                    default -> {
                         System.out.println("NÚMERO INVÁLIDO!");
                         opcaoEscolhida = 0;
+                    }
                 }
             }
             System.out.println("Finalizando o programa...");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void exibirMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
     }
 }
---
+++ src/main/java/br/com/alura/command/ListarPetsCommand.java
@@ -0,0 +1,20 @@
+package br.com.alura.command;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.service.PetService;
+
+import java.io.IOException;
+
+public class ListarPetsCommand implements Command {
+
+    @Override
+    public void execute() {
+        try {
+            ClientHttpConfiguration client = new ClientHttpConfiguration();
+            PetService petService = new PetService(client);
+            petService.listarPets();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
